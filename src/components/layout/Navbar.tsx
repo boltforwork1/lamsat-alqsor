edc88a2bbf1e7508177cc52e9e5801d371dc4709
boltforwork1 <boltforwork1@gmail.com>
2026-03-01 09:11:02 +0200

    Updated Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
           <img
             src="/logo.png"
             alt="Lamsat Al Qosoor Logo"
-            className="h-20 md:h-36 w-auto group-hover:scale-105 transition-transform duration-500"
+            className="h-20 md:h-35 w-auto group-hover:scale-105 transition-transform duration-500"
           />
         </Link>
 
